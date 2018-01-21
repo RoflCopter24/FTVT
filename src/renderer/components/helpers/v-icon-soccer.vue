57321792cd36a722498f0b4d6f2c20d044fa726b
RoflCopter24 <florian@florian-vick.de>
2018-01-21 17:01:55 +0100

    New icon file and fixed background-image path

--- src/renderer/components/helpers/v-icon-soccer.vue
+++ src/renderer/components/helpers/v-icon-soccer.vue
@@ -13,7 +13,7 @@
         display: inline-block;
         width: 24px;
         height: 24px;
-        background: url('/static/Soccer-128.png') no-repeat center;
+        background: url('../../../../static/Soccer-128.png') no-repeat center;
         background-size: contain;
         margin-right: 6px;
     }
